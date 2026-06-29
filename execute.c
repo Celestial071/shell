3c1dd37921f63a4dd0d154a51aa1ea8f8ba7d3cb
Celestial071 <unmeshsubedi6@gmail.com>
2026-06-29 11:47:33 +0545

    added support for cd

--- execute.c
+++ execute.c
@@ -1,20 +1,53 @@
 #include "include/execute.h"
 
 
+int is_builtin(char *cmd) {
+
+  return  strcmp(cmd, "cd") == 0 || 
+          strcmp(cmd, "exit") == 0 ||
+          strcmp(cmd, "pwd") == 0;
+}
+
+
 void executeCommands(char **argv) {
-  pid_t pid = fork();
 
-  if(pid < 0) {
-    perror("fork failed");
-    return;
+  if(argv == NULL && argv[0] == NULL) return ;
+
+  if(is_builtin(argv[0])) {
+    if(strcmp(argv[0], "cd") == 0){
+      builtin_cd(argv);
+    }
+
+  }else{ 
+    pid_t pid = fork();
+
+    if(pid < 0) {
+      perror("fork failed");
+      return;
+    }
+
+    if(pid == 0) {
+      execvp(argv[0], argv);
+      perror("execvp failed");
+      exit(1);
+    } else {
+      int status;
+      waitpid(pid, &status, 0);
+    }
+  }
+}
+
+
+int builtin_cd(char **argv) {
+  if(argv[1] == NULL) {
+    fprintf(stderr, "cd: missing operand\n");
+    return 1;
   }
 
-  if(pid == 0) {
-    execvp(argv[0], argv);
-    perror("execvp failed");
-    exit(1);
-  } else {
-    int status;
-    waitpid(pid, &status, 0);
+  if(chdir(argv[1]) != 0 ) {
+    perror("cd");
+    return 1;
   }
+
+  return 0;
 }
--- include/execute.h
+++ include/execute.h
@@ -6,6 +6,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
+int is_builtin(char *cmd);
 void executeCommands(char **argv);
+
+int builtin_cd(char **argv);
 
 #endif //EXecute_H
